54281b62d9c2bd9b14150d2f565f1547450eec42
Caio Matsui <caiomatsuidev@outlook.com>
2024-02-13 13:44:06 -0300

    debug para falso

--- Matsuiflix/settings.py
+++ Matsuiflix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bujh0mgyf4$oiw7)ly)k!jk3e(!7g_*$4d9%_!#5#53)ya32ij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
