389dc6c9049816df3cf5634e3d5edc4505707837
Caio Matsui <caiomatsuidev@outlook.com>
2024-02-20 08:44:18 -0300

    first commit

--- Matsuiflix/settings.py
+++ Matsuiflix/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Matsuiflix.urls'
@@ -115,7 +114,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
