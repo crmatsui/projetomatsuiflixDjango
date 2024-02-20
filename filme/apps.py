9e1cbd6caa4768d7fc387eab6d30d9f6b29ef966
Caio Matsui <caiomatsuidev@outlook.com>
2024-02-20 14:01:51 -0300

    first commit

--- filme/apps.py
+++ filme/apps.py
@@ -5,3 +5,15 @@ class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
+    def ready(self):
+        from .models import Usuario
+        import os
+
+        email = os.getenv("EMAIL_ADMINISTRADOR")
+        senha = os.getenv("SENHA_ADMINISTRADOR")
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            Usuario.objects.create_superuser(username="admin", email=email, password=senha,
+                                             is_active=True, is_staff=True)
+
