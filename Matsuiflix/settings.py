feba5819cf6258406e95babc9d4a549f20dc274d
Caio Matsui <caiomatsuidev@outlook.com>
2024-02-20 09:20:23 -0300

    first commit

--- Matsuiflix/settings.py
+++ Matsuiflix/settings.py
@@ -30,7 +30,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://projetomatsuiflixdjango-production.up.railway.app/", "localhost", "127.0.0.1" ]
+ALLOWED_HOSTS = ["https://projetomatsuiflixdjango-production.up.railway.app", "localhost", "127.0.0.1" ]
 
 
 # Application definition
